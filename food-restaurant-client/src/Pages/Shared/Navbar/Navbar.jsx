f79e2cb4bde7c7a819444a1f773616a2ba2999d6
tawfiq <tawfiqealahi99@gmail.com>
2024-07-03 01:57:37 +0600

    navbar edit

--- food-restaurant-client/src/Pages/Shared/Navbar/Navbar.jsx
+++ food-restaurant-client/src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,5 +1,17 @@
 
 const Navbar = () => {
+
+    const navOptions = <>
+          <li><a>Item 1</a></li>
+        <li>
+          <a>Parent</a>
+          <ul className="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </li>
+        <li><a>Item 3</a></li>
+     </>
     return (
         <>
         <div className="navbar bg-base-100">
@@ -22,32 +34,19 @@ const Navbar = () => {
       <ul
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+       
+        {navOptions}
+
       </ul>
+
+
+
     </div>
     <a className="btn btn-ghost text-xl">Food Court</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+     {navOptions}
     </ul>
   </div>
   <div className="navbar-end">
